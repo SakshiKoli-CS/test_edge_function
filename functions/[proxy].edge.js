840a3f28eb31af9710ce00bdc123bba4638aeca8
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-09-24 15:15:09 +0530

    remove logs

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,33 +1,21 @@
-const mobileHost = "dynamiclaunch.contentstackapps.com";
-const webHost = "exampleeu.eu-contentstackapps.com"; // Temporary working domain
+const mobileHost = "edge-device-adaptation-mobile.contentstackapps.com";
+const webHost = "testedgefunction.contentstackapps.com";
 
 export default async function handler(request) {
   const userAgentHeader = request.headers.get('User-Agent');
-  const originalUrl = new URL(request.url);
-  
-  const targetUrl = new URL(originalUrl);
+  const targetUrl = new URL(request.url);
   if (isMobile(userAgentHeader)) {
     targetUrl.hostname = mobileHost;
   } else {
     targetUrl.hostname = webHost;
   }
 
-  const headers = new Headers();
-  headers.set("Host", targetUrl.host);
-
-  // Create a completely new request instead of modifying existing one
   const newRequest = new Request(targetUrl.toString(), {
     method: request.method,
-    headers: headers,
+    headers: request.headers,
     body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined
   });
-
-  // Log debug info
-  console.log("User-Agent:", userAgentHeader);
-  console.log("Device:", isMobile(userAgentHeader) ? "Mobile" : "Desktop");
-  console.log("Target URL:", targetUrl.toString());
-  console.log("Host header:", targetUrl.host);
-
+  
   return fetch(newRequest);
 }
 
