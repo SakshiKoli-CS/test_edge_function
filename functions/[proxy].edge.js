5eaabc35f880c83bcc2b183d3e4b5438a6f781e4
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-09-30 12:09:15 +0530

    use await

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -10,15 +10,24 @@ export default async function handler(request) {
     targetUrl.hostname = webHost;
   }
 
-  const newRequest = new Request(targetUrl.toString(), {
-    method: request.method,
-    headers: request.headers,
-    body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined
-  });
-  return fetch(newRequest);
+  const response = await fetch(new Request(targetUrl, request));
+  console.log('response status', response.clone().status);
+  return response;
 }
 
 function isMobile(userAgent) {
   const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
   return mobileRegex.test(userAgent);
 }
+
+
+// const newRequest = new Request(targetUrl.toString(), {
+//   method: request.method,
+//   headers: request.headers,
+//   body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined
+// });
+
+
+
+// const newRequest = new Request(targetUrl.toString(), request);
+// return fetch(newRequest);
