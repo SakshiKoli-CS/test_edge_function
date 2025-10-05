eedc29808ed9ab8777fc885a254fccb2a430cb6e
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-10-06 00:19:10 +0530

    modify fetch call

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -7,7 +7,7 @@ export default async function handler(request) {
   console.log('Fetching data from API:', apiUrl.toString());
   
   try {
-    const apiResponse = await fetch(new Request(apiUrl));
+    const apiResponse = await fetch(apiUrl, new Request(apiUrl));
     const apiData = await apiResponse.json();
     console.log('API data received:', apiData);
     
