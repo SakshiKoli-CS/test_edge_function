7ab605cde55dd82a9d083a4f26f1cbb659a4206f
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-10-13 15:08:00 +0530

    add /test to rewrite

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,6 +1,13 @@
 export default async function handler(request) {
-  const modifiedUrl = new URL(request.url);
-  modifiedUrl.hostname = "edge-device-adaptation-mobile.contentstackapps.com";
-  const newRequest = new Request(modifiedUrl, request);
-  return fetch(newRequest);
+  const url = new URL(request.url);
+
+  if (url.pathname === "/test") {
+    url.hostname = "edge-device-adaptation-mobile.contentstackapps.com";
+
+    const newRequest = new Request(url, request);
+
+    return fetch(newRequest);
+  }
+
+  return fetch(request);
 }
